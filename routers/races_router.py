1253884eba416caf7a9d537c8cf78f6e919ede45
Camill0x <01169209@pw.edu.pl>
2024-01-11 20:16:24 +0100

    implemented checking if race is active

--- routers/races_router.py
+++ routers/races_router.py
@@ -9,6 +9,17 @@ from dtos import RaceDto
 router = APIRouter(prefix="/api/races")
 
 
+@router.get("/active/{race_id}", response_model=bool)
+async def check_race_if_active(race_id: int, repo: Annotated[IRaceRepository, Depends(get_race_repository)]):
+    existing_races = repo.get_races()
+    race = next((element for element in existing_races if element.id == race_id), None)
+
+    if not race:
+        raise HTTPException(status_code=404, detail=f"Race with ID {race_id} not found.")
+
+    return race.isActive
+
+
 @router.post("/")
 async def create_race(race: RaceDto, repo: Annotated[IRaceRepository, Depends(get_race_repository)]):
     for r in repo.get_races():
