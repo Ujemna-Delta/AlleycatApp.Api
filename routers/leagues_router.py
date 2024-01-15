ab774e6fdf76f639489215238455146cdf9a0314
Camill0x <01169209@pw.edu.pl>
2024-01-15 23:44:43 +0100

    added league results by id and userId

--- routers/leagues_router.py
+++ routers/leagues_router.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
-from dtos import LeagueDto
+from dtos import LeagueDto, LeagueResultDto
 from infrastructure import get_league_repository, redirect_response
 from repositories import ILeagueRepository
 
@@ -18,10 +18,61 @@ async def create_league(league: LeagueDto, repo: Annotated[ILeagueRepository, De
 
 
 @router.put("/{league_id}")
-async def update_league(league_id: int, league: LeagueDto, repo: Annotated[ILeagueRepository, Depends(get_league_repository)]):
+async def update_league(league_id: int, league: LeagueDto,
+                        repo: Annotated[ILeagueRepository, Depends(get_league_repository)]):
     for r in repo.get_leagues():
         if r.name == league.name and r.id != league_id:
             raise HTTPException(status_code=409, detail="League with the specified name already exists.")
 
     response = repo.update_league(league_id, league)
     return redirect_response(response)
+
+
+@router.get("/results/{league_id}")
+async def get_league_results(league_id: int, repo: Annotated[ILeagueRepository, Depends(get_league_repository)]):
+    league = repo.get_league_by_id(league_id)
+    if not league:
+        raise HTTPException(status_code=404, detail=f"League with ID {league_id} not found.")
+
+    league_scores = repo.get_league_scores_by_league_id(league_id)
+    sorted_league_scores = sorted(league_scores, key=lambda x: x.score, reverse=True)
+
+    league_results_list = [
+        LeagueResultDto(
+            attendeeId=league_score.attendeeId,
+            rank=rank+1,
+            score=league_score.score,
+            leagueId=league_score.leagueId
+        ).to_dict()
+        for rank, league_score in enumerate(sorted_league_scores)
+    ]
+
+    return league_results_list
+
+
+@router.get("/results/user/{attendee_id}")
+async def get_league_results_by_attendee_id(attendee_id: str,
+                                            repo: Annotated[ILeagueRepository, Depends(get_league_repository)]):
+
+    league_scores = repo.get_league_scores_by_attendee_id(attendee_id)
+    league_ids = [league_score.leagueId for league_score in league_scores]
+
+    league_results_final = []
+    for league_id in league_ids:
+        league_scores = repo.get_league_scores_by_league_id(league_id)
+        sorted_league_scores = sorted(league_scores, key=lambda x: x.score, reverse=True)
+
+        league_results_list = [
+            LeagueResultDto(
+                attendeeId=league_score.attendeeId,
+                rank=rank + 1,
+                score=league_score.score,
+                leagueId=league_score.leagueId
+            ).to_dict()
+            for rank, league_score in enumerate(sorted_league_scores)
+            if league_score.attendeeId == attendee_id
+        ]
+
+        league_results_final.extend(league_results_list)
+
+    return league_results_final
